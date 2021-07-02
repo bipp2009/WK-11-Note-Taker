1e50947d5cac7cb39cd369da820237fa661b2ee8
Bipin <bipp2009@gmail.com>
2021-07-02 15:59:33 +0100

    heroku app deployed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const fs = require("fs");
 
 router.get("/api/notes", (request, response) => {
-  console.log("Get request called");
+//  console.log("Get request called");
   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   response.json(savedNotes);
 });
 
 router.post("/api/notes", (request, response) => {
-  console.log("post request called");
+//  console.log("post request called");
   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   savedNotes.push(request.body);
   fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
